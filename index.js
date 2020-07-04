e003163d554f02af9c6bdf5bf69e699d07adc5ec
kollyd <majaagunoyinkolade@gmail.com>
2020-07-04 03:10:24 +0100

    version 1.0.2

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const emoji = (str) => {
     return `💩${str}💩`
+};
+
+const solveMaths = (a,b)=>{
+    return a+b;
+};
+
+const randomNumberGenerator=(min,max)=>{
+    if(typeof(max)!=='number' && typeof(min) !== 'number'){
+        min = 0; max = 1;
+    };
+    return (Math.random() * (max-min)) + min;
 }
 
-
 module.exports = {
-    emoji
+    emoji,
+    solveMaths,
+    randomNumberGenerator
 }
